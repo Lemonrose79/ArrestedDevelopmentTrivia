6e98d1de7dc043832863c70ccabeedf173457fc6
Leslie Monrose <lesliemonrose@Leslies-MacBook.local>
2020-08-27 18:09:56 -0400

    begin js

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // console.log( "ready!" );
+
+    // track which question we are on
+    var questionCounter = 0;
+    // initial time of 15 seconds for each question
+    var time = 15;
+    // will keep tally of right guesses for end game
+    var correctGuesses = 0;
+    //will keep tally of wrong guesses for end game
+    var incorrectGuesses = 0;
+
+    // question & answer array
+    var questions = [
+      {
